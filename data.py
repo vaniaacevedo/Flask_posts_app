4ef8fa55200888cc6945c0fbefc7687a9649a25d
vaniaacevedo <vaniaacevedo@me.com>
2018-03-24 09:57:17 +0100

    first commit for flask app

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles= [
+        {
+            'title':'Article One',
+            'id':1,
+            'body':'Test text',
+            'author':'Vania Acevedo',
+            'create_date':'04-25-2017'
+        },
+        {
+            'title':'Article Two',
+            'id':2,
+            'body':'Test text',
+            'author':'Angie Acevedo',
+            'create_date':'03-15-2017'
+        },
+        {
+            'title':'Article Three',
+            'id':3,
+            'body':'Test text',
+            'author':'Aida Acevedo',
+            'create_date':'06-23-2017'
+        },
+        {
+            'title':'Article Four',
+            'id':4,
+            'body':'Test text',
+            'author':'Maria Acevedo',
+            'create_date':'07-24-2017'
+        }
+    ]
+    return articles
